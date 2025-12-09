68b397f5d1b174cc5a8d0d77c69d434763a0b1b1
VladCleins <vlad.cleins@yandex.ru>
2025-12-09 14:25:07 +0400

    Practice part 3

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже просмотрели?', '');
+'use strict';
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -9,7 +21,8 @@ const personalMovieDB = {
 };
 
 
-for (let i = 0; i < 2; i++) {
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотренных фильмов?', ''),
           b = prompt('На сколько оцените его?', '');
     if (a != '' && b !='' && a.length < 50 && a != null && b != null) {
@@ -20,17 +33,38 @@ for (let i = 0; i < 2; i++) {
         i--;
     }
 
+    }
 }
 
-if (personalMovieDB.count < 10) {
+// rememberMyFilms();
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
     console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель")
-} else if (personalMovieDB.count >=30) {
-    console.log("Вы киноман")
-} else {
-    console.log("Произошла ошибка")
-};
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель")
+    } else if (personalMovieDB.count >=30) {
+        console.log("Вы киноман")
+    } else {
+        console.log("Произошла ошибка")
+    }
+}
 
+// detectPersonalLevel();
 
-console.log(personalMovieDB);
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++){
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+writeYourGenres();
